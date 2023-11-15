497d2b62dbd851f1fa402e8fca236d1d7b9d6050
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-15 12:06:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 //your JS code here. If required.
 function functionName(){
-    alert(arguments.callee.name)
+    // alert(arguments.callee.name)
+	alert("functionName");
 }
 functionName();
 function AccioJob(){
-    alert(arguments.callee.name)
+    // alert(arguments.callee.name)
+	alert("AccioJob");
 }
 AccioJob();
